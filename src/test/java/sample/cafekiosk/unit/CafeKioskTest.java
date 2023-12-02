210f65f3c1c0640518a647181b118842b5db60c9
SonByeongJoo <thsqudwn05@gmail.com>
2023-12-02 15:43:40 +0900

    섹션4 - DisplayName을 섬세하게

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import sample.cafekiosk.unit.beverage.Americano;
@@ -21,7 +22,10 @@ class CafeKioskTest {
 		System.out.println(">>> 담긴 음료: " + cafeKiosk.getBeverages().get(0).getName());
 	}
 
+	// Junit4라면 메서드 네임을 한글로 명시적으로 적어준다.
 	@Test
+//	@DisplayName("음료 1개 추가 테스트")
+	@DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
 	void add() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		cafeKiosk.add(new Americano());
