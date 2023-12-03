617cbc5f6266c6c4d9a4cf0394fb0cc6f01bbde9
SonByeongJoo <thsqudwn05@gmail.com>
2023-12-03 23:55:01 +0900

    섹션5 - Business Layer 테스트 (1)

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,31 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+	private final ProductRepository productRepository;
+	private final OrderRepository orderRepository;
+
+	public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
+		List<String> productNumbers = request.getProductNumbers();
+		//Product
+		List<Product> products = productRepository.findByProductNumberIn(productNumbers);
+
+		Order order = Order.create(products, registeredDateTime);
+
+		return OrderResponse.of(order);
+	}
+}
